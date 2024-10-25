cf8bec3ee88b1b2d5b01e8c77b9cba63769899d0
timofey.verzunov <timofey.verzunov@axenix.pro>
2024-10-25 11:27:32 +0300

    load project 25.10.2024

---
+++ src/main/java/tvgu/tversu/map/mapUniversity/MapUniversityApplication.java
@@ -0,0 +1,13 @@
+package tvgu.tversu.map.mapUniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapUniversityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapUniversityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tvgu/tversu/map/mapUniversity/MapUniversityApplicationTests.java
@@ -0,0 +1,13 @@
+package tvgu.tversu.map.mapUniversity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapUniversityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
